fc8ad61cef2ecc54100d81ccebf63700c150dee9
Yoshiki <mas03151997@gmail.com>
2024-08-21 03:39:38 +0900

    add local for CHiME-4

---
+++ egs2/chime4/speechlm1/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+sample_rate=16000
+
+train_set=tr05_simu_isolated_1ch_track
+valid_set=dt05_simu_isolated_1ch_track
+test_sets="et05_simu_isolated_1ch_track"
+
+codec_opts="--codec_choice ESPnet --codec_hf_model_tag espnet/owsmdata_soundstream_16k_200epoch"
+
+./speechlm.sh \
+    --task "se" \
+    --data_name "chime4" \
+    --fs $sample_rate \
+    --nj 16 \
+    --inference_nj 16 \
+    --audio_format "wav.ark" \
+    --train_config conf/tuning/train_delay.yaml \
+    --inference_config conf/decode_asr_debug.yaml \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    $codec_opts \
+    "$@"
